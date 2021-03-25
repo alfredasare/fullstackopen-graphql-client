59ffbb8aec395eebe64e8edfc45211bc517e981e
alfredasare <alfredasare101@gmail.com>
2021-03-25 12:03:48 +0000

    Use useLazyQuery to get data on request

--- src/components/Persons.js
+++ src/components/Persons.js
@@ -1,10 +1,58 @@
+import {useState, useEffect} from "react";
+import {gql, useLazyQuery} from "@apollo/client";
+
+const FIND_PERSON = gql`
+    query findPersonByName($nameToSearch: String!) {
+        findPerson(name: $nameToSearch) {
+            name
+            phone
+            id
+            address {
+                city
+                street
+            }
+        }
+    }
+`;
+
 const Persons = ({persons}) => {
+    const [getPerson, result] = useLazyQuery(FIND_PERSON);
+    const [person, setPerson] = useState(null);
+
+    const showPerson = name => {
+        getPerson({
+            variables: {
+                nameToSearch: name
+            }
+        });
+    };
+
+    useEffect(() => {
+        if (result.data) {
+            setPerson(result.data.findPerson);
+        }
+    }, [result]);
+
+    if (person) {
+        return(
+            <div>
+                <h2>{person.name}</h2>
+                <div>{person.address.street} {person.address.city}</div>
+                <div>{person.phone}</div>
+                <button onClick={() => setPerson(null)}>close</button>
+            </div>
+        )
+    }
+
     return (
         <div>
             <h2>Persons</h2>
             {persons.map(p =>
                 <div key={p.name}>
                     {p.name} {p.phone}
+                    <button onClick={() => showPerson(p.name)} >
+                        show address
+                    </button>
                 </div>
             )}
         </div>
