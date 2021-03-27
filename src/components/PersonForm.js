c519e72b4980817d807ff5a217b05fdd56ca3c98
alfredasare <alfredasare101@gmail.com>
2021-03-27 22:05:27 +0000

    Update cache after adding person

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -10,11 +10,18 @@ const PeronForm = ({setError}) => {
     const [city, setCity] = useState('');
 
     const [createPerson] = useMutation(CREATE_PERSON, {
-        refetchQueries: [{
-            query: ALL_PERSONS
-        }],
         onError: (error) => {
             setError(error.graphQLErrors[0].message);
+        },
+        update: (store, response) => {
+            const dataInStore = store.readQuery({query: ALL_PERSONS});
+            store.writeQuery({
+                query: ALL_PERSONS,
+                data: {
+                    ...dataInStore,
+                    allPersons: [...dataInStore.allPersons, response.data.addPerson]
+                }
+            });
         }
     });
 
